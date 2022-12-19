e802a8055e1d630f218fec502b3baa98932dd3f5
BennetMontgomery <bennetlogan@gmail.com>
2022-12-19 17:58:55 -0500

    Added W&B integration

--- validate.py
+++ validate.py
@@ -50,20 +50,40 @@ npcs=[
 def plot_validate():
     for i in range(len(configs)):
         rewards = []
+        rewards_matrix = {
+            "time":[],
+            "ilc":[],
+            "motion":[],
+            "reversing":[],
+            "collision":[],
+            "goal":[],
+            "exit":[],
+            "timeout":[]
+        }
 
         for network in networks:
-            reward = ego.validate(configs[i], validation_folder, npcs=npcs[i], network=network)
+            reward, reward_matrix = ego.validate(configs[i], validation_folder, npcs=npcs[i], network=network, split_reward=True)
 
             rewards.append(reward)
 
+            for key in reward_matrix.keys():
+                rewards_matrix[key].append(reward_matrix[key])
+
         plt.plot(domain, rewards)
-        plt.title(f"{configs[i]} performance over time")
+        plt.title(f"total {configs[i]} performance over time")
         plt.savefig(f"./{configs[i]}.png")
         plt.close()
 
+        for key in rewards_matrix.keys():
+            plt.plot(domain, rewards_matrix[key])
+            plt.title(f"reward for {key} in {configs[i]} over time")
+            plt.savefig(f"./{key}_{configs[i]}.png")
+            plt.close()
+
 def graphically_validate(confg_idx=0, network="2022-11-14 19:05:20.309803_100"):
-    reward = ego.validate(configs[confg_idx], validation_folder, npcs=npcs[confg_idx], network=network, graphical_mode=True)
+    reward, reward_matrix = ego.validate(configs[confg_idx], validation_folder, npcs=npcs[confg_idx], network=network, graphical_mode=True, split_reward=True)
 
-    return reward
+    return reward, reward_matrix
 
-print(graphically_validate(3, "2022-11-16 02:50:48.228403_1900"))
+# print(graphically_validate(3, "2022-11-16 02:50:48.228403_1900"))
+plot_validate()
