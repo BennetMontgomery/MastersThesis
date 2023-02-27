a56eb795de5564f9cd2673c60cabadb12755670a
Bennet Montgomery <bennet.montgomery@queensu.ca>
2023-02-27 00:23:36 +0000

    Starting DQN-encoder connection rewiring

--- agents/behaviour_net.py
+++ agents/behaviour_net.py
@@ -49,7 +49,7 @@ class BehaviourNet(tf.keras.Model):
         return self.replay_manager.sample_batch(batch_size)
 
     def select_action(self, obs, step):
-        # get the probabilitiy of selecting a random action instead of e-greedy from policy
+        # get the probability of selecting a random action instead of e-greedy from policy
         rate = math.exp(-1 * step * self.e_decay)
 
         # convert obs to tensor
@@ -99,3 +99,57 @@ class BehaviourNet(tf.keras.Model):
 
         # return distribution of choice weighting for actions
         return q_output
+    
+    
+class ModifiedBehaviourNet(tf.keras.Model):
+    def __init__(self,
+                 static_input_size, variable_input_size,
+                 attention_in_d, q_layers, pooler_layers, attention_out_ff, attention_heads,
+                 memory_cap, e_decay=0.001, dropout_rate=0.01):
+        super(BehaviourNet, self).__init__()
+
+        self.variable_input_size = variable_input_size
+        self.static_input_size = static_input_size
+        self.attention_in_d = attention_in_d
+        self.e_decay = e_decay
+        self.q_layers = q_layers
+
+        # construct sublayers
+        # encoder
+        self.encoder = Encoder(attention_in_d, attention_heads, attention_out_ff, dropout_rate, variable_input_size)
+        
+        # static embedder
+        self.static_embedder = tf.keras.layers.Embedding(200, attention_in_d, mask_zero=True)
+
+        # Q subnet
+        # self.q_subnet = DQN(q_layers, input_s=((static_input_size*2)*attention_in_d,))
+        self.q_subnet = tf.keras.Sequential()
+
+        # build memory buffer
+        self.replay_manager = ReplayManager(replay_cap=memory_cap)
+        
+    def add_mem(self, experience):
+        self.replay_manager.add_mem(experience)
+
+    def sample_replay_batch(self, batch_size):
+        return self.replay_manager.sample_batch(batch_size)
+
+    def select_action(self, obs, step):
+        # get the probability of selecting a random action instead of e-greedy from policy
+        rate = math.exp(-1 * step * self.e_decay)
+
+        # convert obs to tensor
+        try:
+            obs = tf.convert_to_tensor(obs, dtype='float32')
+        except:
+            for l in obs:
+                print(l)
+            exit(1)
+
+        if rate > random.random():
+            # return random action, exploration rate at current step, indicator that action was random
+            return random.randrange(self.q_layers[-1]), rate, False
+        else:
+            # return argmax_a q(s, a), rate at current step, indicator that action was not random
+            # print("selecting action without failure")
+            return np.argmax(self(obs)), rate, True
